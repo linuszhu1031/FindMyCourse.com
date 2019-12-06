8944ffc75f9be06e32df342385f9075f4a0f71ae
Linus Zhu <linuszhu1031@gmail.com>
2019-12-06 04:38:28 -0600

    finished all prof pages and professor filter, removed and added proper pictures

--- professor-display.js
+++ professor-display.js
@@ -1,7 +1,195 @@
 $(function() {
+  $(".selectDepartmentList").on('click', 'li a', function() {
+    $(".selectDepartment:first-child").text($(this).text());
+    $(".selectDepartment:first-child").val($(this).text());
+  });
+});
 
-    $(".selectArea").click(function() {
-      $(".btn:first-child").text($(this).text());
-      $(".btn:first-child").val($(this).text());
-   });
+$(function() {
+  $(".selectAreaList").on('click', 'li a', function() {
+    $(".selectArea:first-child").text($(this).text());
+    $(".selectArea:first-child").val($(this).text());
+  });
+});
+
+$(function() {
+  $("#cs").click(function() {
+    $("#fleck").show();
+    $("#green").hide();
+    $("#huang").hide();
+    $("#lazebnik").show();
+    $("#smaragdis").show();
+  });
+});
+
+$(function() {
+  $("#cs").click(function() {
+    $("#ai").click(function() {
+      $("#fleck").show();
+      $("#green").hide();
+      $("#huang").hide();
+      $("#lazebnik").show();
+      $("#smaragdis").hide();
+    });
+  });
+});
+
+$(function() {
+  $("#cs").click(function() {
+    $("#compv").click(function() {
+      $("#fleck").hide();
+      $("#green").hide();
+      $("#huang").hide();
+      $("#lazebnik").show();
+      $("#smaragdis").hide();
+    });
+  });
+});
+
+$(function() {
+  $("#cs").click(function() {
+    $("#sig").click(function() {
+      $("#fleck").hide();
+      $("#green").hide();
+      $("#huang").hide();
+      $("#lazebnik").hide();
+      $("#smaragdis").show();
+    });
+  });
+});
+
+$(function() {
+  $("#ece").click(function() {
+    $("#fleck").hide();
+    $("#green").hide();
+    $("#huang").show();
+    $("#lazebnik").hide();
+    $("#smaragdis").show();
+  });
+});
+
+$(function() {
+  $("#ece").click(function() {
+    $("#ai").click(function() {
+      $("#fleck").hide();
+      $("#green").hide();
+      $("#huang").show();
+      $("#lazebnik").hide();
+      $("#smaragdis").hide();
+    });
+  });
+});
+
+$(function() {
+  $("#ece").click(function() {
+    $("#compv").click(function() {
+      $("#fleck").hide();
+      $("#green").hide();
+      $("#huang").show();
+      $("#lazebnik").hide();
+      $("#smaragdis").hide();
+    });
+  });
+});
+
+$(function() {
+  $("#ece").click(function() {
+    $("#sig").click(function() {
+      $("#fleck").hide();
+      $("#green").hide();
+      $("#huang").hide();
+      $("#lazebnik").hide();
+      $("#smaragdis").show();
+    });
+  });
+});
+
+$(function() {
+  $("#fin").click(function() {
+    $("#fleck").hide();
+    $("#green").hide();
+    $("#huang").hide();
+    $("#lazebnik").hide();
+    $("#smaragdis").hide();
+  });
+});
+
+$(function() {
+  $("#ling").click(function() {
+    $("#fleck").hide();
+    $("#green").show();
+    $("#huang").hide();
+    $("#lazebnik").hide();
+    $("#smaragdis").hide();
+  });
+});
+
+$(function() {
+  $("#ling").click(function() {
+    $("#neuro").click(function() {
+      $("#fleck").hide();
+      $("#green").show();
+      $("#huang").hide();
+      $("#lazebnik").hide();
+      $("#smaragdis").hide();
+    });
+  });
+});
+
+$(function() {
+  $("#ling").click(function() {
+    $("#ai").click(function() {
+      $("#fleck").hide();
+      $("#green").hide();
+      $("#huang").hide();
+      $("#lazebnik").hide();
+      $("#smaragdis").hide();
+    });
+  });
+});
+
+$(function() {
+  $("#ling").click(function() {
+    $("#compv").click(function() {
+      $("#fleck").hide();
+      $("#green").hide();
+      $("#huang").hide();
+      $("#lazebnik").hide();
+      $("#smaragdis").hide();
+    });
+  });
+});
+
+$(function() {
+  $("#ling").click(function() {
+    $("#corp-fin").click(function() {
+      $("#fleck").hide();
+      $("#green").hide();
+      $("#huang").hide();
+      $("#lazebnik").hide();
+      $("#smaragdis").hide();
+    });
+  });
+});
+
+$(function() {
+  $("#ling").click(function() {
+    $("#sig").click(function() {
+      $("#fleck").hide();
+      $("#green").hide();
+      $("#huang").hide();
+      $("#lazebnik").hide();
+      $("#smaragdis").hide();
+    });
+  });
+});
+
+$(function() {
+  $("#yid").click(function() {
+    $("#fleck").hide();
+    $("#green").hide();
+    $("#huang").hide();
+    $("#lazebnik").hide();
+    $("#smaragdis").hide();
+  });
 });
